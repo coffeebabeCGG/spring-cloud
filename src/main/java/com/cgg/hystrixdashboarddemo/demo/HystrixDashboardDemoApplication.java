b7f8c9773e278a7c0ace3f519aeef2678a0ffe57
fg <cuig@koal.com>
2021-04-13 15:20:43 +0800

    First commit

---
+++ src/main/java/com/cgg/hystrixdashboarddemo/demo/HystrixDashboardDemoApplication.java
@@ -0,0 +1,13 @@
+package com.cgg.hystrixdashboarddemo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HystrixDashboardDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cgg/hystrixdashboarddemo/demo/HystrixDashboardDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cgg.hystrixdashboarddemo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HystrixDashboardDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
